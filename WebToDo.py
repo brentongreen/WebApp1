1bf1f0fb54e37aae1ac4757ed2e7bb49d748f186
brent <brenton.green@gmail.com>
2024-09-26 17:35:19 +0300

    Web App version 1

---
+++ WebToDo.py
@@ -0,0 +1,25 @@
+import streamlit as st
+import functions
+
+todos = functions.get_todos()
+
+def add_todo():
+    new_todo = st.session_state["new_todo"] + "\n"
+    todos.append(new_todo)
+    functions.write_todos(todos)
+
+st.title("My Web App")
+st.subheader("My to do list")
+st.write("Enter Items")
+
+for index, todo in enumerate(todos):
+    check_box = st.checkbox(todo, key=index)
+    if check_box:
+        todos.pop(index)
+        functions.write_todos(todos)
+        del st.session_state[index]
+        st.rerun()
+
+st.text_input(label="",key="new_todo",placeholder="Enter data",on_change=add_todo)
+
+st.session_state
